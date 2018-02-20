30d4d6e6a497f6654e9dd6fe4f4a12e2f86e6641
MaribelNavia <maribelnavia21@gmail.com>
2018-02-20 10:35:24 -0600

    activity--js-functions-basic

--- ex-01-sumMinZero.js
+++ ex-01-sumMinZero.js
@@ -3,6 +3,12 @@
 //   Write a function called sumMinZero that returns zero
 //   if the sum of two numbers is negative
 //
+	function sumMinZero(num1, num2){
+		if ((num1 + num2) < 0){
+			return 0
+		}
+	}
+
 //------------------------------------------------------------------
 
 console.assert(sumMinZero(10, 10) === 20)
--- ex-02-getInput.js
+++ ex-02-getInput.js
@@ -6,12 +6,16 @@
 //  if the sum of two numbers is negative
 //
 //------------------------------------------------------------------
-
-console.assert(sumMinZero(10, 10) === 20)
-console.assert(sumMinZero(30, 10) === 40)
-console.assert(sumMinZero(-30, 10) === 0)
-console.assert(sumMinZero(-9893, -8839) === 0)
-console.assert(sumMinZero(1848, -4124) === 0)
+//function sumMinZero(num1, num2){
+	//if ((num1 + num2) < 0){
+//		return 0
+//	}
+//}
+//console.assert(sumMinZero(10, 10) === 20)
+//console.assert(sumMinZero(30, 10) === 40)
+//console.assert(sumMinZero(-30, 10) === 0)
+//console.assert(sumMinZero(-9893, -8839) === 0)
+//console.assert(sumMinZero(1848, -4124) === 0)
 
 //Warmup - JS Drills - Basic Functions & Data Types
 
@@ -23,8 +27,19 @@ console.assert(sumMinZero(1848, -4124) === 0)
 //  Examine the functionality of `getInput` in the function below
 //------------------------------------------------------------------
 
+function getInput(input){
+	if (typeof input === "undefined"){
+		return "Please provide string input."	
+	} else if (typeof input === "number"){
+		return "Please provide a string input, NOT a number"
+	} else{
+		return "DATA_RECEIVED: " + input 
+	}
+}
+
+
 
 console.assert(getInput() === "Please provide string input.")
 console.assert(getInput(333) === "Please provide a string input, NOT a number")
 console.assert(getInput('Mister Teatime') === "DATA_RECEIVED: Mister Teatime")
-console.assert(getInput('Charlotte') === "DATA_RECEIVED: Charolette")
+console.assert(getInput('Charlotte') === "DATA_RECEIVED: Charlotte")
